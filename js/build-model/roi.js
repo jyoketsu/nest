328f6ac1f1563aaf7b4a9f2bc565a1997bc6b119
jyoketsu <jyoketsu@163.com>
2017-07-12 17:08:58 +0800

    20170712

--- js/build-model/roi.js
+++ js/build-model/roi.js
@@ -1758,6 +1758,7 @@ function unSelectGraph() {
         clearContext("content",this.previousSelectedGraph.coverageId);
         drawGraph(previousSelectedGraph);
         previousSelectedGraph = null;
+        graphSelected = false;
     }
 }
 
